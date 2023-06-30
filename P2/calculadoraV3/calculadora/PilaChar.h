8c6259636f691506521b6398547aaad56c1db96d
Daniel Aviles <daaviles3@espe.edu.ec>
2023-06-30 18:43:09 -0500

    Calculadora V3

---
+++ P2/calculadoraV3/calculadora/PilaChar.h
@@ -0,0 +1,25 @@
+#pragma once
+#include "NodoChar.h"
+#include <iostream>
+#include <string>
+using namespace std;
+class PilaChar {
+private:
+	NodoChar* primero;
+	NodoChar* actual;
+public:
+	//métodos de los atributos
+	PilaChar();
+	void setPrimero(NodoChar*);
+	void setActual(NodoChar*);
+	NodoChar* getPrimero();
+	NodoChar* getActual();
+	bool getPilaVacia();
+	//métodos de orden de la lista
+	void push(char);
+	void pop();
+	void mostrar();
+	void buscar(char);
+	int contar();
+	//void buscar(int);
+};
